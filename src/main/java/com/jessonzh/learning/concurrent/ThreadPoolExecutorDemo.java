253d038f06d71d8371055085386c6bd26e3191c5
Jesson Zhou <zhjesson@gmail.com>
2020-04-09 00:03:34 +0800

    线程池回顾

---
+++ src/main/java/com/jessonzh/learning/concurrent/ThreadPoolExecutorDemo.java
@@ -0,0 +1,46 @@
+package com.jessonzh.learning.concurrent;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolExecutorDemo {
+    public static void main(String[] args) {
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(5,
+                12,
+                1,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(7),
+                new ThreadPoolExecutor.AbortPolicy());
+        for (int i = 0; i < 30; i++) {
+            poolExecutor.execute(new Thread(() -> {
+                // 线程操作
+                System.out.println(Thread.currentThread().getName());
+//                try {
+//                    TimeUnit.SECONDS.sleep(2);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }, "Thread-" + String.valueOf(i)));
+        }
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 30; i++) {
+            poolExecutor.execute(new Thread(() -> {
+                // 线程操作
+                System.out.println(Thread.currentThread().getName());
+//                try {
+//                    TimeUnit.SECONDS.sleep(2);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }, "Thread-" + String.valueOf(i)));
+        }
+
+        //终止线程池
+        poolExecutor.shutdown();
+    }
+}
