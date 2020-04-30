63ba7b795a80aeadb0f6bd000107d7996811e285
Jesson Zhou <zhjesson@gmail.com>
2020-04-30 18:32:47 +0800

    HyperLogLog Demo

---
+++ src/main/java/com/jessonzh/learning/redis/HyperLogLogTest.java
@@ -0,0 +1,23 @@
+package com.jessonzh.learning.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.Jedis;
+
+/**
+ * HyperLogLog测试
+ * @author Administrator
+ */
+@Slf4j(topic = "HyperLogLogTest")
+public class HyperLogLogTest {
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        log.info("start pfadd");
+        for (int i = 0; i < 100000; i++) {
+            jedis.pfadd("hlltest.com", "user" + i);
+        }
+        long count = jedis.pfcount("hlltest.com");
+        jedis.close();
+        log.info(String.valueOf(count));
+    }
+}
